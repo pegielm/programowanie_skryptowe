a72321cc2f6f6b95a25c6436a5eff0572a16d361
pegielm <pegielm@gmail.com>
2023-11-12 14:47:15 +0100

    lab4

--- lab4/main.py
+++ lab4/main.py
@@ -3,8 +3,9 @@ from model import Vector2d, MoveDirection
 from controller import Simulation, OptionsParser
 
 directions: list[MoveDirection] = OptionsParser.parse(sys.argv[1:]) 
-positions: list[Vector2d] = [Vector2d(2,2), Vector2d(3,4)] # Pozycje początkowe dla zwierząt, odpowiednio, (2,2) oraz (3,4) 
+#positions: list[Vector2d] = [Vector2d(2,2), Vector2d(3,4)] # Pozycje początkowe dla zwierząt, odpowiednio, (2,2) oraz (3,4) 
+positions: list[Vector2d] = [Vector2d(2,4), Vector2d(3,4),Vector2d(0,4)] # Pozycje początkowe dla zwierząt, odpowiednio, (2,2) oraz (3,4) 
 simulation: Simulation = Simulation(directions, positions)
 simulation.run()
 
-# python .\main.py f b r l f f r r f f f f f f f f
+# python3 .\main.py f b r l f f r r f f f f f f f f
