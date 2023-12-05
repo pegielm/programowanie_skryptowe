0bde609c4025c98366252462c9511167348dfa89
pegielm <pegielm@gmail.com>
2023-12-05 21:57:39 +0100

    xd

--- lab8/zadanie1.js
+++ lab8/zadanie1.js
@@ -1,4 +1,18 @@
 
-function sum(x,y){  
-    return x + y;
+function readtype() {
+    for (let i = 0; i < 4; i++) {
+        let inputValue = window.prompt("Wprowadź wartość:");
+        console.log(`wczytanaWartość: ${inputValue}, typWczytanejWartości: ${typeof inputValue}`);
+    }
 }
+
+function funkcja_zwrotna() {
+    let inputTextValue = document.forms[0].elements[0].value;
+    let inputNumberValue = document.forms[0].elements[1].value;
+    console.log(`wczytanaWartośćZPolaTekstowego: ${inputTextValue}, typWczytanejWartości: ${typeof inputTextValue}`);
+    console.log(`wczytanaWartośćZPolaNumerycznego: ${inputNumberValue}, typWczytanejWartości: ${typeof inputNumberValue}`);
+}
+function eventHandler(event) {
+    console.log(event);
+}
+
